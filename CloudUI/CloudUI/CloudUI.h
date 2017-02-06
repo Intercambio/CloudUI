1427c23f6ffd14518db311ef1a93d55cf536002a
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-02-06 14:23:56 +0100

    Initial Commit

---
+++ CloudUI/CloudUI/CloudUI.h
@@ -0,0 +1,19 @@
+//
+//  CloudUI.h
+//  CloudUI
+//
+//  Created by Tobias Kraentzer on 06.02.17.
+//  Copyright © 2017 Tobias Kräntzer. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for CloudUI.
+FOUNDATION_EXPORT double CloudUIVersionNumber;
+
+//! Project version string for CloudUI.
+FOUNDATION_EXPORT const unsigned char CloudUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudUI/PublicHeader.h>
+
+
