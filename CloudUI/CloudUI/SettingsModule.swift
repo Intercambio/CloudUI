2133d474dac1bf2ad5af144b0df39282fa49eaf0
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-02-08 16:21:53 +0100

    Setup internal components of the settings module

--- CloudUI/CloudUI/SettingsModule.swift
+++ CloudUI/CloudUI/SettingsModule.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Fountain
 import CloudStore
 
 public class SettingsModule: UserInterfaceModule {
@@ -17,6 +18,111 @@ public class SettingsModule: UserInterfaceModule {
     }
     
     public func makeViewController() -> UIViewController {
-        return UIViewController()
+        let presenter = SettingsPresenter(cloudService: cloudService)
+        return SettingsViewController(presenter: presenter)
+    }
+}
+
+protocol SettingsView: class {
+    var dataSource: FTDataSource? { get set }
+}
+
+class SettingsViewController: FormViewController, SettingsView {
+    let presenter: SettingsPresenter
+    init(presenter: SettingsPresenter) {
+        self.presenter = presenter
+        super.init(style: .grouped)
+        self.presenter.view = self
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+    }
+}
+
+extension SettingsViewController: SettingsUserInterface {
+    public func presentSettings(for account: CloudService.Account, animated: Bool) {
+        presenter.account = account
+    }
+}
+
+class SettingsPresenter {
+    
+    let cloudService: CloudService
+    init(cloudService: CloudService) {
+        self.cloudService = cloudService
+    }
+    
+    weak var view: SettingsView? {
+        didSet {
+            view?.dataSource = dataSource
+        }
+    }
+    
+    var account: CloudService.Account? {
+        didSet {
+            guard
+                let account = self.account
+                else {
+                dataSource = nil
+                return
+            }
+            
+            dataSource = SettingsDataSource(cloudService: cloudService, account: account)
+        }
+    }
+    
+    var dataSource: FTDataSource? {
+        didSet {
+            view?.dataSource = dataSource
+        }
+    }
+}
+
+class SettingsDataSource: NSObject, FTDataSource {
+    
+    let cloudService: CloudService
+    let account: CloudService.Account
+    public init(cloudService: CloudService, account: CloudService.Account) {
+        self.cloudService = cloudService
+        self.account = account
+        super.init()
+    }
+    
+    // MARK: - FTDataSource
+    
+    public func numberOfSections() -> UInt {
+        return 1
+    }
+    
+    public func numberOfItems(inSection section: UInt) -> UInt {
+        return 0
+    }
+    
+    public func sectionItem(forSection section: UInt) -> Any! {
+        return nil
+    }
+    
+    public func item(at indexPath: IndexPath!) -> Any! {
+        return nil
+    }
+    
+    private let _observers: NSHashTable = NSHashTable<FTDataSourceObserver>.weakObjects()
+    
+    public func observers() -> [Any]! {
+        return _observers.allObjects
+    }
+    
+    public func addObserver(_ observer: FTDataSourceObserver!) {
+        if _observers.contains(observer) == false {
+            _observers.add(observer)
+        }
+    }
+    
+    public func removeObserver(_ observer: FTDataSourceObserver!) {
+        _observers.remove(observer)
     }
 }
