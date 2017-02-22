4b281a6b28044d991738e98f3bb28fd95906ea14
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-02-22 11:10:39 +0100

    Add progress button

---
+++ CloudUI/CloudUI/ProgressView.swift
@@ -0,0 +1,90 @@
+//
+//  ProgressView.swift
+//  CloudUI
+//
+//  Created by Tobias Kräntzer on 22.02.17.
+//  Copyright © 2017 Tobias Kräntzer. All rights reserved.
+//
+
+import UIKit
+
+class ProgressView: UIView {
+
+    dynamic var progress: Progress? {
+        didSet{
+            setNeedsDisplay()
+        }
+    }
+    
+    private var context = "ProgressView.Context"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupDisplayLink()
+        setupObserver()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupDisplayLink()
+        setupObserver()
+    }
+    
+    private func setupObserver() {
+        addObserver(self,
+                    forKeyPath: "progress.fractionCompleted",
+                    options: [],
+                    context: &context)
+        addObserver(self,
+                    forKeyPath: "progress.isIndeterminate",
+                    options: [],
+                    context: &context)
+    }
+    
+    private var displayLink: CADisplayLink?
+    private func setupDisplayLink() {
+        let displayLink = CADisplayLink(target: self, selector: #selector(updateProgress))
+        displayLink.add(to: RunLoop.main, forMode: .commonModes)
+        self.displayLink = displayLink
+    }
+    
+    @objc private func updateProgress() {
+        setNeedsDisplay()
+    }
+    
+    deinit {
+        displayLink?.invalidate()
+        removeObserver(self, forKeyPath: "progress.fractionCompleted")
+        removeObserver(self, forKeyPath: "progress.isIndeterminate")
+    }
+    
+    override func tintColorDidChange() {
+        super.tintColorDidChange()
+        setNeedsDisplay()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard
+            context == &self.context
+            else {
+                super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+                return
+        }
+        
+        DispatchQueue.main.async {
+            self.displayLink?.isPaused = !(self.progress?.isIndeterminate ?? false)
+            self.setNeedsDisplay()
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        StyleKit.drawProgressCircle(frame: bounds,
+                                    resizing: .aspectFit,
+                                    color: tintColor,
+                                    backgroundColor: UIColor.lightGray,
+                                    progress: CGFloat(progress?.fractionCompleted ?? 0),
+                                    isIndeterminate: progress?.isIndeterminate ?? true,
+                                    phase: CGFloat(Date.timeIntervalSinceReferenceDate))
+    }
+}
